7f92aaf897e1a12bd59aa06805c92f9ee629b566
Paulo Gabriel Landim <gableit@outlook.com>
2026-06-16 00:04:45 -0300

    feat: implementa parser do mapa e exclusao mutua nas celulas

--- src/main.c
+++ src/main.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
 #include "modules/map/map.h"
-
-
+#include <stdio.h>
 
 int main() {
 
   Map *meu_mapa = load_map("mapa.txt");
   if (meu_mapa != NULL) {
-    printf("Mapa carregado com sucesso! Linhas: %d, Colunas: %d\n", meu_mapa->rows, meu_mapa->columns);
+    printf("Mapa carregado com sucesso! Linhas: %d, Colunas: %d\n",
+           meu_mapa->rows, meu_mapa->columns);
 
-  destroy(meu_mapa);
-  return 0;
-
+    destroy(meu_mapa);
+    return 0;
   }
 }

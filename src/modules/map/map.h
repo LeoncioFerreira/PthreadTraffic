ccadde100367279a56b708880b4a546cb4536fd8
Paulo Gabriel Landim <gableit@outlook.com>
2026-06-16 07:48:46 -0300

    style: aplica clang-format nos arquivos do mapa

--- src/modules/map/map.h
+++ src/modules/map/map.h
@@ -3,33 +3,33 @@
 
 #include <pthread.h>
 /**
- * Descrição: Define as principais estruturas de dados necessárias para o funcionamento do mapa, como o tipo de terreno (CellType), a célula (Cell - que armazena a direção e garante a exclusão mútua através do Mutex) e a matriz principal do Mapa (Map).
- * Autor: Paulo Gabriel
+ * Descrição: Define as principais estruturas de dados necessárias para o
+ * funcionamento do mapa, como o tipo de terreno (CellType), a célula (Cell -
+ * que armazena a direção e garante a exclusão mútua através do Mutex) e a
+ * matriz principal do Mapa (Map). Autor: Paulo Gabriel
  */
 
-
 typedef enum celltype {
-    EMPTY,
-    ROADS,
-    INTERSECTION,
-}CellType;
+  EMPTY,
+  ROADS,
+  INTERSECTION,
+} CellType;
 
 typedef struct cell {
-    CellType type; // empty, road or intersection
-    char direction; // 'N' (Norte), 'S' (Sul), 'L' (Leste), 'O' (Oeste) ou ' ' (vazio)
-    pthread_mutex_t mutex;
+  CellType type;  // empty, road or intersection
+  char direction; // 'N' (Norte), 'S' (Sul), 'L' (Leste), 'O' (Oeste) ou ' '
+                  // (vazio)
+  pthread_mutex_t mutex;
 
-}Cell;
-
+} Cell;
 
 typedef struct map {
-    int rows; //lines
-    int columns;
-    Cell **cell_grid;
-}Map;
+  int rows; // lines
+  int columns;
+  Cell **cell_grid;
+} Map;
 
-
-Map* load_map(const char *path_file);
+Map *load_map(const char *path_file);
 void destroy(Map *mapa);
 
 #endif

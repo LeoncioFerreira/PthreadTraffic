83d02fc2bd27476af6b474004d6678342fea618d
LandimPG <gableit@outlook.com>
2026-06-25 10:48:41 -0300

    style: formata traffic.h ignorado pelo make format

--- src/modules/traffic/traffic.h
+++ src/modules/traffic/traffic.h
@@ -1,31 +1,28 @@
 #ifndef TRAFFIC_H
 #define TRAFFIC_H
 
+#include "../map/map.h"
 #include <pthread.h>
 #include <stdbool.h>
-#include "../map/map.h"
 
 /**
  * Descrição: Definição das estruturas de dados do subsistema de semáforos,
- * representando os estados de trânsito nos cruzamentos e as primitivas de 
+ * representando os estados de trânsito nos cruzamentos e as primitivas de
  * sincronização para os veículos.
  * Autor: Paulo
  */
 
-typedef enum {
-    LIGHT_HORIZ_GREEN,
-    LIGHT_VERT_GREEN
-} LightState;
+typedef enum { LIGHT_HORIZ_GREEN, LIGHT_VERT_GREEN } LightState;
 
 typedef struct {
-    int id;
-    int row;
-    int col;
-    LightState state;
-    int toggle_ticks;
+  int id;
+  int row;
+  int col;
+  LightState state;
+  int toggle_ticks;
 
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
+  pthread_mutex_t mutex;
+  pthread_cond_t cond;
 } TrafficLight;
 
 void traffic_init(const Map *map, int default_toggle_ticks);
